62ee23bbd4056f214dabb58cb8bb9a4fb343559e
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2019-10-01 12:15:37 +0200

    Add files via upload

---
+++ robertJanjaninDorucak/robertJanjaninDorucak/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace robertJanjaninDorucak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkDorucak = new System.Windows.Forms.CheckBox();
+            this.chkRucak = new System.Windows.Forms.CheckBox();
+            this.chkVecera = new System.Windows.Forms.CheckBox();
+            this.btnPosalji = new System.Windows.Forms.Button();
+            this.txtPrikaz = new System.Windows.Forms.TextBox();
+            this.chkSakrij = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // chkDorucak
+            // 
+            this.chkDorucak.AutoSize = true;
+            this.chkDorucak.Location = new System.Drawing.Point(110, 99);
+            this.chkDorucak.Name = "chkDorucak";
+            this.chkDorucak.Size = new System.Drawing.Size(67, 17);
+            this.chkDorucak.TabIndex = 0;
+            this.chkDorucak.Text = "Dorucak";
+            this.chkDorucak.UseVisualStyleBackColor = true;
+            // 
+            // chkRucak
+            // 
+            this.chkRucak.AutoSize = true;
+            this.chkRucak.Location = new System.Drawing.Point(110, 176);
+            this.chkRucak.Name = "chkRucak";
+            this.chkRucak.Size = new System.Drawing.Size(58, 17);
+            this.chkRucak.TabIndex = 1;
+            this.chkRucak.Text = "Rucak";
+            this.chkRucak.UseVisualStyleBackColor = true;
+            // 
+            // chkVecera
+            // 
+            this.chkVecera.AutoSize = true;
+            this.chkVecera.Location = new System.Drawing.Point(110, 263);
+            this.chkVecera.Name = "chkVecera";
+            this.chkVecera.Size = new System.Drawing.Size(60, 17);
+            this.chkVecera.TabIndex = 2;
+            this.chkVecera.Text = "Vecera";
+            this.chkVecera.UseVisualStyleBackColor = true;
+            // 
+            // btnPosalji
+            // 
+            this.btnPosalji.Location = new System.Drawing.Point(281, 350);
+            this.btnPosalji.Name = "btnPosalji";
+            this.btnPosalji.Size = new System.Drawing.Size(265, 91);
+            this.btnPosalji.TabIndex = 3;
+            this.btnPosalji.Text = "Posalji";
+            this.btnPosalji.UseVisualStyleBackColor = true;
+            this.btnPosalji.Click += new System.EventHandler(this.BtnPosalji_Click);
+            // 
+            // txtPrikaz
+            // 
+            this.txtPrikaz.Location = new System.Drawing.Point(264, 97);
+            this.txtPrikaz.Multiline = true;
+            this.txtPrikaz.Name = "txtPrikaz";
+            this.txtPrikaz.Size = new System.Drawing.Size(308, 210);
+            this.txtPrikaz.TabIndex = 4;
+            this.txtPrikaz.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // chkSakrij
+            // 
+            this.chkSakrij.AutoSize = true;
+            this.chkSakrij.Location = new System.Drawing.Point(358, 327);
+            this.chkSakrij.Name = "chkSakrij";
+            this.chkSakrij.Size = new System.Drawing.Size(97, 17);
+            this.chkSakrij.TabIndex = 5;
+            this.chkSakrij.Text = "sakrij narudzbu";
+            this.chkSakrij.UseVisualStyleBackColor = true;
+            this.chkSakrij.CheckedChanged += new System.EventHandler(this.ChkSakrij_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chkSakrij);
+            this.Controls.Add(this.txtPrikaz);
+            this.Controls.Add(this.btnPosalji);
+            this.Controls.Add(this.chkVecera);
+            this.Controls.Add(this.chkRucak);
+            this.Controls.Add(this.chkDorucak);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkDorucak;
+        private System.Windows.Forms.CheckBox chkRucak;
+        private System.Windows.Forms.CheckBox chkVecera;
+        private System.Windows.Forms.Button btnPosalji;
+        private System.Windows.Forms.TextBox txtPrikaz;
+        private System.Windows.Forms.CheckBox chkSakrij;
+    }
+}
+
---
+++ robertJanjaninDorucak/robertJanjaninDorucak/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace robertJanjaninDorucak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChkSakrij_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!chkSakrij.Checked) {
+                if (chkDorucak.Checked)
+                {
+                    txtPrikaz.AppendText("Odabrali ste dorucak");
+
+                }
+                if (chkRucak.Checked)
+                {
+                    txtPrikaz.AppendText("\r\nOdabrali ste rucak");
+                }
+                if (chkVecera.Checked)
+                {
+                    txtPrikaz.AppendText("\r\nOdabrali ste veceru");
+                }
+
+            }
+            else
+            {
+                txtPrikaz.Clear();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnPosalji_Click(object sender, EventArgs e)
+        {
+            if (chkSakrij.Checked)
+            { }
+            else
+            {
+
+                txtPrikaz.Clear();
+                if (chkDorucak.Checked)
+                {
+                    txtPrikaz.AppendText("Odabrali ste dorucak");
+
+                }
+                if (chkRucak.Checked)
+                {
+                    txtPrikaz.AppendText("\r\nOdabrali ste rucak");
+                }
+                if (chkVecera.Checked)
+                {
+                    txtPrikaz.AppendText("\r\nOdabrali ste veceru");
+                }
+            }
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
